fff095d1419c38ef6698f16f3f9719d862fec6bf
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-19 18:12:31 +0900

    First commit for initialization of project. Tapping of the sceneview code and testing

--- Whack_a_jellyfish/ViewController.swift
+++ Whack_a_jellyfish/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    @IBAction func reset(_ sender: Any) {
+    }
+    func addNode(){
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in:sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty{
+            print("Didnt touch anything")
+        }
+        else{
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
+        
+    }
 }
 
